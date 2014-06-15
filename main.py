ef14abeedfde5ce16f77effe73c86bbfc25ff239
Michael Fogleman <fogleman@gmail.com>
2014-06-14 22:29:38 -0400

    added model.repeat(), support dual polygons

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from math import sin, cos, tan, pi, atan2
 import cairo
 
 BACKGROUND_COLOR = 0x000000
+DUAL = False
 MARGIN = 0.1
 SCALE = 64
 SHOW_LABELS = False
@@ -22,6 +23,9 @@ def color(value):
     b = ((value >> 0) & 0xff) / 255.0
     return (r, g, b)
 
+def normalize(x, y):
+    return (round(x, 6), round(y, 6))
+
 class Shape(object):
     def __init__(self, sides, x=0, y=0, rotation=0, **kwargs):
         self.sides = sides
@@ -32,6 +36,11 @@ class Shape(object):
         self.stroke = STROKE_COLOR
         for key, value in kwargs.items():
             setattr(self, key, value)
+    def copy(self, x, y):
+        return Shape(
+            self.sides, x, y, self.rotation,
+            fill=self.fill, stroke=self.stroke
+        )
     def points(self, margin=0):
         angle = 2 * pi / self.sides
         rotation = self.rotation - pi / 2
@@ -82,11 +91,21 @@ class Shape(object):
         dc.move_to(x, y)
         dc.show_text(text)
 
+class DualShape(Shape):
+    def __init__(self, points):
+        super(DualShape, self).__init__(len(points) - 1)
+        self.data = points
+    def points(self, margin=0):
+        return self.data
+
 class Model(object):
     def __init__(self):
         self.shapes = []
+        self.lookup = {}
     def append(self, shape):
         self.shapes.append(shape)
+        key = normalize(shape.x, shape.y)
+        self.lookup[key] = shape
     def add(self, index, edge, sides, **kwargs):
         parent = self.shapes[index]
         shape = parent.adjacent(sides, edge, **kwargs)
@@ -98,36 +117,31 @@ class Model(object):
                 self.add(index, edge, sides, **kwargs)
         end = len(self.shapes)
         return range(start, end)
-    def render(self, dc, x=0, y=0):
-        dc.save()
-        dc.translate(x, y)
-        if SHOW_LABELS:
-            for shape in self.shapes:
-                shape.render_edge_labels(dc)
-        for index, shape in enumerate(self.shapes):
-            shape.render(dc)
-            if SHOW_LABELS:
-                shape.render_label(dc, index)
-        dc.restore()
-    def _recursive_render(self, dc, indexes, x, y, depth, memo):
+    def add_repeats(self, x, y):
+        for shape in self.shapes:
+            key = normalize(x + shape.x, y + shape.y)
+            if key in self.lookup:
+                continue
+            self.lookup[key] = shape.copy(x + shape.x, y + shape.y)
+    def _repeat(self, indexes, x, y, depth, memo):
         if depth < 0:
             return
-        key = (round(x, 6), round(y, 6))
+        key = normalize(x, y)
         previous_depth = memo.get(key, -1)
         if previous_depth >= depth:
             return
         memo[key] = depth
         if previous_depth == -1:
-            self.render(dc, x, y)
+            self.add_repeats(x, y)
         for index in indexes:
             shape = self.shapes[index]
-            self._recursive_render(
-                dc, indexes, x + shape.x, y + shape.y, depth - 1, memo)
-    def recursive_render(self, dc, indexes):
+            self._repeat(
+                indexes, x + shape.x, y + shape.y, depth - 1, memo)
+    def repeat(self, indexes):
         memo = {}
         depth = 0
         while True:
-            self._recursive_render(dc, indexes, 0, 0, depth, memo)
+            self._repeat(indexes, 0, 0, depth, memo)
             d = SIZE / 2.0 / SCALE
             tl = any(x < -d and y < -d for x, y in memo)
             tr = any(x > d and y < -d for x, y in memo)
@@ -136,6 +150,32 @@ class Model(object):
             if tl and tr and bl and br:
                 break
             depth += 1
+    def dual(self):
+        vertexes = {}
+        for shape in self.lookup.values():
+            for (x, y) in shape.points()[:-1]:
+                key = normalize(x, y)
+                vertexes.setdefault(key, []).append(shape)
+        result = []
+        for (x, y), shapes in vertexes.items():
+            if len(shapes) < 3:
+                continue
+            def angle(shape):
+                return atan2(shape.y - y, shape.x - x)
+            shapes.sort(key=angle)
+            points = [(shape.x, shape.y) for shape in shapes]
+            points.append(points[0])
+            result.append(DualShape(points))
+        return result
+    def render(self, dc):
+        shapes = self.dual() if DUAL else self.lookup.values()
+        if SHOW_LABELS:
+            for shape in shapes:
+                shape.render_edge_labels(dc)
+        for index, shape in enumerate(shapes):
+            shape.render(dc)
+            if SHOW_LABELS:
+                shape.render_label(dc, index)
 
 def main(pattern):
     surface = cairo.ImageSurface(cairo.FORMAT_RGB24, SIZE, SIZE)
@@ -154,7 +194,8 @@ def main(pattern):
         model.append(Shape(6))
         a = model.add_all([0], range(6), 3)
         b = model.add_all(a, [1], 6)
-        model.recursive_render(dc, b)
+        model.repeat(b)
+        model.render(dc)
 
     if pattern == 1: # 4.6.12
         model = Model()
@@ -162,7 +203,8 @@ def main(pattern):
         a = model.add_all([0], range(0, 12, 2), 6, fill=ORANGE)
         b = model.add_all([0], range(1, 12, 2), 4, fill=WHITE)
         c = model.add_all(b, [2], 12, fill=RED)
-        model.recursive_render(dc, c)
+        model.repeat(c)
+        model.render(dc)
 
     if pattern == 2: # 3.3.4.3.4
         model = Model()
@@ -171,7 +213,8 @@ def main(pattern):
         b = model.add_all(a, [1], 4)
         c = model.add_all(b, [2, 3], 3)
         d = model.add_all(c, [2], 4)
-        model.recursive_render(dc, d)
+        model.repeat(d)
+        model.render(dc)
 
     if pattern == 3: # 3.3.3.3.6
         model = Model()
@@ -180,14 +223,16 @@ def main(pattern):
         b = model.add_all(a, [1], 3)
         c = model.add_all(a, [2], 3)
         d = model.add_all(c, [1], 6, fill=RED)
-        model.recursive_render(dc, d)
+        model.repeat(d)
+        model.render(dc)
 
     if pattern == 4: # 4.8.8
         model = Model()
         model.append(Shape(8, fill=RED))
         a = model.add_all([0], range(1, 8, 2), 4)
         b = model.add_all(a, [1], 8, fill=RED)
-        model.recursive_render(dc, b)
+        model.repeat(b)
+        model.render(dc)
 
     if pattern == 5: # 3.3.4.12 / 3.3.3.3.3.3
         model = Model()
@@ -196,7 +241,8 @@ def main(pattern):
         b = model.add_all([0], range(1, 12, 2), 4)
         c = model.add_all(b, [1, 3], 3)
         d = model.add_all(b, [2], 12, fill=RED)
-        model.recursive_render(dc, d)
+        model.repeat(d)
+        model.render(dc)
 
     if pattern == 6: # 3.4.6.4
         model = Model()
@@ -204,7 +250,8 @@ def main(pattern):
         a = model.add_all([0], range(6), 4)
         b = model.add_all(a, [1], 3)
         c = model.add_all(a, [2], 6, fill=RED)
-        model.recursive_render(dc, c)
+        model.repeat(c)
+        model.render(dc)
 
     if pattern == 7: # 3.3.4.4
         model = Model()
@@ -213,14 +260,16 @@ def main(pattern):
         b = model.add_all(a, [1, 3], 3)
         c = model.add_all(b, [1], 3)
         d = model.add_all(c, [2], 4)
-        model.recursive_render(dc, d)
+        model.repeat(d)
+        model.render(dc)
 
     if pattern == 8: # 3.3.3.3.3.3
         model = Model()
         model.append(Shape(3))
         a = model.add_all([0], range(3), 3)
         b = model.add_all(a, [1, 2], 3)
-        model.recursive_render(dc, b)
+        model.repeat(b)
+        model.render(dc)
 
     if pattern == 9:
         model = Model()
@@ -241,14 +290,16 @@ def main(pattern):
         e = model.add_all(d, [2], 6, fill=RED)
         model.add_all(a, [1], 3)
         model.add_all(c, [1], 3)
-        model.recursive_render(dc, e)
+        model.repeat(e)
+        model.render(dc)
 
     if pattern == 11:
         model = Model()
         model.append(Shape(8))
         a = model.add_all([0], range(0, 8, 2), 6)
         b = model.add_all(a, [3], 8)
-        model.recursive_render(dc, b)
+        model.repeat(b)
+        model.render(dc)
 
     surface.write_to_png('output.png')
 
